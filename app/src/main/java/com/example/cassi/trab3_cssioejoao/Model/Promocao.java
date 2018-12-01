7cf8debcd59906a3b0c76acf543349bca71471cd
Cassio Reis <cassio_2000@hotmail.com>
2018-12-01 06:17:50 -0800

    Models Promocao

--- app/src/main/java/com/example/cassi/trab3_cssioejoao/Model/Promocao.java
+++ app/src/main/java/com/example/cassi/trab3_cssioejoao/Model/Promocao.java
@@ -1,4 +1,28 @@
 package com.example.cassi.trab3_cssioejoao.Model;
 
 public class Promocao {
+
+    private String nome;
+    double valor;
+
+    public Promocao(String nome, double valor) {
+        this.nome = nome;
+        this.valor = valor;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
 }
