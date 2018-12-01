a67aeac7603b192e41a1ea228cf9d9a718dd7733
Cassio Reis <cassio_2000@hotmail.com>
2018-12-01 06:56:01 -0800

    Promocao Contract feita

--- app/src/main/java/com/example/cassi/trab3_cssioejoao/PromocaoContract.java
+++ app/src/main/java/com/example/cassi/trab3_cssioejoao/PromocaoContract.java
@@ -1,4 +1,21 @@
 package com.example.cassi.trab3_cssioejoao;
 
-public class PromocaoContract {
+
+import android.provider.BaseColumns;
+
+public final class PromocaoContract {
+    public final class Promocao implements BaseColumns {
+        public final static String TABLE_NAME = "Promocao";
+        public static final String COLUMN_NAME_ID = "_ID";
+        public final static String COLUMN_NAME_NOME = "nome";
+        public static final String COLUMN_NAME_VALOR = "valor";
+
+        public final static String CREATE_PROMOCAO  = "CREATE TABLE "+Promocao.TABLE_NAME+" ("
+                + Promocao.COLUMN_NAME_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+                + Promocao.COLUMN_NAME_NOME + " TEXT, "
+                + Promocao.COLUMN_NAME_VALOR + " TEXT"
+                +")";
+
+        public final static String DROP_PROMOCAO = "DROP TABLE IF EXISTS "+Promocao.TABLE_NAME;
+    }
 }
