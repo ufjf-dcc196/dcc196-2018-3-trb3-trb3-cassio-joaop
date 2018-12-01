4e85f093e836df745f951803df98c71db739a83e
Cassio Reis <cassio_2000@hotmail.com>
2018-12-01 06:02:04 -0800

    Telas Promocao e Criar Promocao

---
+++ app/src/main/java/com/example/cassi/trab3_cssioejoao/FormPromocao.java
@@ -0,0 +1,13 @@
+package com.example.cassi.trab3_cssioejoao;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FormPromocao extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_form_promocao);
+    }
+}
